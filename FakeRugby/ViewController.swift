102022150a8601964350e35f8efcb99ba0f290bf
Michal Juscinski <mjuscinski0172@jh-mba158-16.d214.org>
2018-03-01 08:18:09 -0600

    Initial Commit

---
+++ FakeRugby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FakeRugby
+//
+//  Created by Michal Juscinski on 3/1/18.
+//  Copyright © 2018 Michal Juscinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
