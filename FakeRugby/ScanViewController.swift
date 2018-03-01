62c6fa9349ab25ab630b505c9e7288b2ef005dfe
Jimmy Rodriguez <jrodriguez8064@jh-mba158-12.d214.org>
2018-03-01 08:49:42 -0600

    created the viewController and ScanVC

---
+++ FakeRugby/ScanViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ScanViewController.swift
+//  FakeRugby
+//
+//  Created by Jimmy Rodriguez on 3/1/18.
+//  Copyright © 2018 Michal Juscinski. All rights reserved.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
